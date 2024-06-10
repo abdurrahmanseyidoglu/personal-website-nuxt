9102851b6b4e8fef6dd1ab22e526c5ddebda651e
Abdurrahman Seyidoğlu <abdurrahmanseyidoglu@gmail.com>
2024-06-10 22:17:53 +0300

    fix twitter card and ts warnings

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,31 +1,38 @@
 <template>
-  <ContentRenderer :value="data ?? {}" class="prose mx-auto dark:prose-invert"/>
+  <ContentRenderer :value="data ?? {}" class="prose mx-auto dark:prose-invert" />
   <div class="tags-wrapper">
-    <template v-if="data?.tags?.length > 0">
-      <Tag v-for="tag in data?.tags" :key="tag" :title="tag"/>
+    <template v-if="data?.tags?.length??[].length > 0">
+      <!-- <Tag v-for="tag in data?.tags" :key="tag" :title="tag"/> -->
     </template>
   </div>
-
 </template>
 
 <script setup lang="ts">
-const {path} = useRoute()
-const {data} = await useAsyncData(`${path}`, () =>
-    queryContent().where({
-      _path: path
-    }).findOne())
+interface ContentData {
+  title?: string;
+  description?: string;
+  cover?: string;
+  tags?: string[];
+}
+
+const { path } = useRoute()
+const { data } = await useAsyncData<ContentData>(`${path}`, () =>
+  queryContent().where({
+    _path: path
+  }).findOne())
+
+const contentData = data as Ref<ContentData | null>
 
 useHead({
-  title: data?.title ? `${data.title} | Abdurrahman` : "Abdurrahman",
+  title: contentData.value?.title ? `${contentData.value.title} | Abdurrahman` : "Abdurrahman",
 })
-// !TODO : Work on the SEO and fix the errors here
-// !TODO : description and ogDescription are not working properly
-// !TODO : fix ogImage and twitterCard
+// TODO: description and ogDescription are not working properly
+
 useSeoMeta({
-  ogTitle: data?.title ? `${data.title} | Abdurrahman` : "Abdurrahman",
-  description: data?.description ? data.description : "Abdurrahman\'s Personal website",
-  ogDescription: data?.description ? data.description : "Abdurrahman\'s Personal website",
-  ogImage: data?.cover ? data.cover : '/images/my-icon.png',
-  twitterCard: data?.cover ? data.cover : '/images/my-icon.png',
+  ogTitle: contentData.value?.title ? `${contentData.value.title} | Abdurrahman` : "Abdurrahman",
+  description: contentData.value?.description ? contentData.value.description : "Abdurrahman\'s Personal website",
+  ogDescription: contentData.value?.description ? contentData.value.description : "Abdurrahman\'s Personal website",
+  ogImage: contentData.value?.cover ? `/images/blog/${contentData.value.cover}` : '/images/my-icon.png',
+  twitterCard: 'summary_large_image',
 })
 </script>
