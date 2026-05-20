de2bf88090314331d67184f27499cce9553bf460
Abdurrahman Seyidoğlu <abdurrahmanseyidoglu@gmail.com>
2026-05-20 15:34:37 +0300

    push build files

--- .output/public/_nuxt/BeGMrYdT.js
+++
@@ -1,1 +0,0 @@
-import{A as a}from"./Bt8pQEgS.js";const i=()=>{const n=a("animationEnabled",()=>!1);return{animationEnabled:n,toggleAnimations:()=>{n.value=!n.value},setAnimations:t=>{n.value=t}}};export{i as u};
---
+++ .output/public/_nuxt/DKOvWyt2.js
@@ -0,0 +1,1 @@
+import{A as a}from"./64JI4jrA.js";const i=()=>{const n=a("animationEnabled",()=>!1);return{animationEnabled:n,toggleAnimations:()=>{n.value=!n.value},setAnimations:t=>{n.value=t}}};export{i as u};
