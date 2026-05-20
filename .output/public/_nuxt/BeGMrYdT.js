ea4656c842f0226c7a28ffb49b13461f4e773e30
Abdurrahman Seyidoğlu <abdurrahmanseyidoglu@gmail.com>
2026-05-20 15:11:06 +0300

    chore: production build

---
+++ .output/public/_nuxt/BeGMrYdT.js
@@ -0,0 +1,1 @@
+import{A as a}from"./Bt8pQEgS.js";const i=()=>{const n=a("animationEnabled",()=>!1);return{animationEnabled:n,toggleAnimations:()=>{n.value=!n.value},setAnimations:t=>{n.value=t}}};export{i as u};
